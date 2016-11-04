448d8f58818fe072e178bad5e2d86b61686bc31b
Andre Herculano <aherculano@babbel.com>
2016-11-04 17:13:55 +0100

    add docs

--- portal/mix.exs
+++ portal/mix.exs
@@ -2,12 +2,20 @@ defmodule Portal.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :portal,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :portal,
+      version: "0.0.1",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      
+      source_url: "https://github.com/andresilveira/elixir_stuff/tree/master/portal",
+      homepage_url: "http://codebikeandmore.com",
+      docs: [
+        extras: ["README.md"]
+      ]
+   ]
   end
 
   # Configuration for the OTP application
@@ -28,6 +36,8 @@ defmodule Portal.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.14", only: :dev}
+    ]
   end
 end
