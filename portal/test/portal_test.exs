f28d80bb9f75b55db2c5b6d90cc08cdc847d95de
Andre Herculano <aherculano@babbel.com>
2016-11-04 16:26:51 +0100

    add simple test for Portal

--- portal/test/portal_test.exs
+++ portal/test/portal_test.exs
@@ -1,8 +1,30 @@
 defmodule PortalTest do
   use ExUnit.Case
   doctest Portal
-
-  test "the truth" do
-    assert 1 + 1 == 2
+  
+  setup_all do
+    { :ok,
+      orange: Portal.shoot(:orange),
+      blue: Portal.shoot(:blue),
+      portal: Portal.transfer(:orange, :blue, [1,2,3])
+    }
+  end
+  
+  test "initialy orange has all the data (but in inverse order)" do
+    assert Portal.Door.get(:orange) == [3,2,1]
+    assert Portal.Door.get(:blue)   == []
+  end
+  
+  test "when we push_right data is moved from orange to blue", state do
+    Portal.push_right(state[:portal])
+    assert Portal.Door.get(:orange) == [2,1]
+    assert Portal.Door.get(:blue)   == [3]
+  end
+  
+  test "when we push_left data is moved from blue to orange", state do
+    Portal.push_left(state[:portal])
+    
+    assert Portal.Door.get(:orange) == [3,2,1]
+    assert Portal.Door.get(:blue)   == []
   end
 end
